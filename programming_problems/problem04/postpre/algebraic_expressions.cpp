d2f6045ebe432b2b97171e8532d00d34ca1655d9
Jillian <jpz15@pitt.edu>
2024-02-14 00:53:07 -0500

    need to add more tests

--- programming_problems/problem04/postpre/algebraic_expressions.cpp
+++ programming_problems/problem04/postpre/algebraic_expressions.cpp
@@ -1,8 +1,8 @@
 #include <string>
 using std::string;
-
+#include <stack>
 #include <iostream>
-
+#include <vector>
 #include <cctype> // for isalpha
 
 #include "algebraic_expressions.hpp"
@@ -41,8 +41,43 @@ bool isPost(string s) {
   return (firstChar == 0);
 }
 
-void convert(string &postfix, string &prefix) {
-
-  // TODO
-  
+void convert(const std::string &postfix, std::string &prefix) {
+    
+    std::stack<std::vector<std::string>> stack;
+    //precondition
+    if(isPost(postfix)){
+    //iterate through every character of the postfix
+    for (char i : postfix) {
+      //if it isnt an operator than it must be an operand
+      if (!isoperator(i)) { 
+        //push it on top of the stack by converting it to a single string element i
+        stack.push(std::vector<std::string>{std::string(1, i)});
+      } 
+      //operands need to pop the top two elements in order to perform the calculation
+      else if (isoperator(i)) {  
+        //need to hold onto the operator but pop it from the stack to get the two operands
+        std::vector<std::string> operate;
+        operate.push_back(std::string(1, i));
+        //this assigns the most recent operand value to the second operand
+        std::vector<std::string> op2 = stack.top();
+        //this removes the top entry
+        stack.pop();
+        //assigns the second (but now it is the most recent entry to the first operand)
+        std::vector<std::string> op1 = stack.top();
+        //this removes it from the top
+        stack.pop();
+        
+        //populating the operator with each operand after the operator
+        operate.insert(operate.end(), op1.begin(), op1.end());
+        operate.insert(operate.end(), op2.begin(), op2.end());
+        //pushes it back onto the stack but in reverse order as the post fix notation
+        stack.push(operate);
+        }
+    }
+    //loops through the stack to assign back to the prefix notation
+    std::vector<std::string> final = stack.top();
+    for (const std::string &str : final) {
+      prefix += str;
+    }
+}
 }
